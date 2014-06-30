b78028edf70775bdbff77e75650ace8762bfe3ec
Natalie Gregory <gregorynr@gmail.com>
2014-06-30 10:03:04 -0600

    week4assess

---
+++ lib/dog_html.rb
@@ -0,0 +1,44 @@
+=begin
+Notes to discern WTF I'm really trying to do here and
+understand what I think this test is "testing" or trying to do.do
+  ?What is h1? No idea what this is. Is it Heading1?
+
+I think there may need to be a puts to ask user to type in a
+string that is converted into an array and then we run tests
+on it, but I'm not certain.
+
+1. Create a method that returns the total number of dogs
+in the hash. AND make it so the H1 (header) reads "There are 4 dogs"
+2. Create a method that returns the total number of dogs
+in the hash. AND make it so the H1 (header) reads "There are 4 dogs"
+end
+end
+
+=end
+
+class DogHTMLPage
+
+  def title (string)
+    "<h1>#{string}</h1>"
+  end
+end
+
+puts "What are the names of your dogs?"
+    dogs_name=gets.chomp
+
+#create user input as an array of hashes.
+words=dogs_name.split
+
+name = hash.new(0)
+name.each do |x,y|
+  if dogs_name=4
+    puts '<h1>There are 4 dogs</h1>'
+
+  if dogs_name =
+
+  if dogs_name = 0
+    puts ""<h1>There are no dogs</h1>"
+  end
+end
+
+end
